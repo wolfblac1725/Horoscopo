9656018de65290a387a3577bb9aa95cb82b7daf7
erikcanseco <wolfblac@gmail.com>
2023-05-04 17:18:35 -0600

    Se agrega navegación de la aplicación

--- app/src/main/java/com/example/horoscopos/MainActivity.kt
+++ app/src/main/java/com/example/horoscopos/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.horoscopos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscopos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContent) as NavHostFragment
+        navController =  navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
